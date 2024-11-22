92aef59da05076e25cd28cf464e84d73ffe065a0
gabrielmanica <gabriel.manica@ciss.com.br>
2024-11-22 11:21:09 -0300

    feat: configura tela de splash

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,32 @@
+import { useFonts } from 'expo-font';
 import { Stack } from 'expo-router';
+import { useEffect } from 'react';
+import * as SplashScreen from 'expo-splash-screen';
+
+// Prevent the splash screen from auto-hiding before asset loading is complete.
+SplashScreen.preventAutoHideAsync();
+
+// Set the animation options. This is optional.
+SplashScreen.setOptions({
+    duration: 1000,
+    fade: true
+});
 
 export default function RootLayout() {
+    const [loaded] = useFonts({
+        SpaceMono: require('../assets/fonts/SpaceMono-Regular.ttf')
+    });
+
+    useEffect(() => {
+        if (loaded) {
+            SplashScreen.hideAsync();
+        }
+    }, [loaded]);
+
+    if (!loaded) {
+        return null;
+    }
+
     return (
         <Stack
             screenOptions={{
