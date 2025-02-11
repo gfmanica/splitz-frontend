fef5eb6f2d4ff40ae26f3ebfb8e346d06de6f490
gabrielmanica <gabriel.manica@ciss.com.br>
2025-02-11 01:06:45 -0300

    feat: implementa validação de formulário de login com Zod e React Hook Form

--- app/(login)/index.tsx
+++ app/(login)/index.tsx
@@ -4,10 +4,48 @@ import { colors } from '@/constants/Colors';
 import { router } from 'expo-router';
 import { Text, View, Image, StyleSheet, TextInput } from 'react-native';
 import { SafeAreaView } from 'react-native-safe-area-context';
-
+import { useForm, Controller } from 'react-hook-form';
+import { zodResolver } from '@hookform/resolvers/zod';
+import { z } from 'zod';
+import { useMutation } from '@tanstack/react-query';
+import { Axios } from '@/lib/axios';
 const logo = require('@/assets/images/logo.png');
 
+// Cria esquema com Zod para validação
+const loginSchema = z.object({
+    email: z
+        .string({ required_error: 'Campo é obrigatório' })
+        .email('Email inválido'),
+    password: z
+        .string({ required_error: 'Campo é obrigatório' })
+        .min(3, 'Senha deve ter no mínimo 3 caracteres')
+});
+
+type LoginFormInputs = z.infer<typeof loginSchema>;
+
 export default function LoginScreen() {
+    const {
+        control,
+        handleSubmit,
+        formState: { errors }
+    } = useForm<LoginFormInputs>({
+        resolver: zodResolver(loginSchema)
+    });
+
+    const { mutate, error } = useMutation({
+        mutationFn: (data: LoginFormInputs) => Axios.post('/login', data),
+        onSuccess: (response) => {
+            const token = response.data.token;
+            Axios.defaults.headers.common['Authorization'] = `${token}`;
+
+            router.push('/bill');
+        }
+    });
+
+    const onSubmit = (data: LoginFormInputs) => {
+        mutate(data);
+    };
+
     return (
         <SafeAreaView style={styles.mainContainer}>
             <View>
@@ -15,27 +53,58 @@ export default function LoginScreen() {
             </View>
 
             <View style={styles.formContainer}>
-                <View style={{ gap: 8 }}>
-                    <Text>E-mail</Text>
+                <Controller
+                    control={control}
+                    name="email"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <>
+                            <Text>E-mail</Text>
+                            <TextInput
+                                keyboardType="email-address"
+                                placeholder="Digite seu e-mail..."
+                                style={styles.textInput}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                            {errors.email && (
+                                <Text style={styles.errorText}>
+                                    {errors.email.message}
+                                </Text>
+                            )}
+                        </>
+                    )}
+                />
 
-                    <TextInput
-                        keyboardType="email-address"
-                        placeholder="Digite seu e-mail..."
-                        style={styles.textInput}
-                    />
-                </View>
+                <Controller
+                    control={control}
+                    name="password"
+                    render={({ field: { onChange, onBlur, value } }) => (
+                        <>
+                            <Text>Senha</Text>
+                            <TextInput
+                                secureTextEntry
+                                placeholder="Digite sua senha..."
+                                style={styles.textInput}
+                                onBlur={onBlur}
+                                onChangeText={onChange}
+                                value={value}
+                            />
+                            {errors.password && (
+                                <Text style={styles.errorText}>
+                                    {errors.password.message}
+                                </Text>
+                            )}
+                        </>
+                    )}
+                />
+                {error && (
+                    <Text style={styles.errorText}>
+                        E-mail ou senha incorretos
+                    </Text>
+                )}
 
-                <View style={{ gap: 8 }}>
-                    <Text>Senha</Text>
-
-                    <TextInput
-                        secureTextEntry
-                        placeholder="Digite sua senha..."
-                        style={styles.textInput}
-                    />
-                </View>
-
-                <Button text="Entrar" onPress={() => router.push('/bill')} />
+                <Button text="Entrar" onPress={handleSubmit(onSubmit)} />
 
                 <LoginDivider />
 
@@ -85,5 +154,10 @@ const styles = StyleSheet.create({
     textFooter: {
         alignSelf: 'center',
         color: colors.neutral[500]
+    },
+    errorText: {
+        color: 'red',
+        fontSize: 12,
+        marginTop: 4
     }
 });
