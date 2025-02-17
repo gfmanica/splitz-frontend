74573b8881616baeca378c4bd3f3d064ea3ae921
gabrielmanica <gabriel.manica@ciss.com.br>
2025-02-16 22:56:01 -0300

    feat: refatora o formulário de contas para integrar o schema de validação e otimizar a lógica de submissão

--- app/(tabs)/bill/form.tsx
+++ app/(tabs)/bill/form.tsx
@@ -5,29 +5,13 @@ import { SaveBlock } from '@/components/bill/form/save-block';
 import { useLocalSearchParams } from 'expo-router';
 import { useMutation, useQuery, useQueryClient } from '@tanstack/react-query';
 import { Axios } from '@/lib/axios';
-import { Bill } from '@/types/types';
+import { Bill, BillFormValues, billSchema } from '@/types/types';
 import { Text, View } from 'tamagui';
 import { colors } from '@/constants/Colors';
 import { FormProvider, useForm } from 'react-hook-form';
-import { z } from 'zod';
 import { zodResolver } from '@hookform/resolvers/zod';
 import { Alert, ScrollView } from 'react-native';
 import { useEffect } from 'react';
-
-const billSchema = z.object({
-    idBill: z.number().optional(),
-    dsBill: z.string().min(1, 'Nome é obrigatório'),
-    vlBill: z.number().min(0.01, 'Valor deve ser no mínimo 0,01'),
-    qtPerson: z.number().min(0.01, 'Quantidade deve ser no mínimo 0,01'),
-    payments: z.array(
-        z.object({
-            dsPerson: z.string().min(1, 'Nome da pessoa é obrigatório'),
-            vlPayment: z.number().min(0.01, 'Valor deve ser no mínimo 0,01')
-        })
-    )
-});
-
-type BillFormValues = z.infer<typeof billSchema>;
 
 export default function BillFormScreen() {
     const { id } = useLocalSearchParams();
@@ -49,15 +33,16 @@ export default function BillFormScreen() {
         }
     });
 
-    console.log(methods.formState.errors);
-
     useEffect(() => methods.reset(data), [data]);
+
+    console.log(methods.formState.errors);
 
     const mutation = useMutation({
         mutationFn: (formData: BillFormValues) =>
             data ? Axios.put('/bill', formData) : Axios.post('/bill', formData),
         onSuccess: () => {
             Alert.alert('Sucesso', 'Conta salva com sucesso!');
+
             queryClient.invalidateQueries({
                 queryKey: ['bills']
             });
@@ -66,9 +51,9 @@ export default function BillFormScreen() {
             Alert.alert('Erro', error.message || 'Erro ao salvar a conta')
     });
 
-    const onSubmit = methods.handleSubmit((formData) => {
-        mutation.mutate(formData);
-    });
+    const onSubmit = methods.handleSubmit((formData) =>
+        mutation.mutate(formData)
+    );
 
     return (
         <View style={{ flex: 1 }}>
